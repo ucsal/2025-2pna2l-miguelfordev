fc615bbd86c6ee7584c9dfc38a61a14e1ae1eee9
Miguel Lyra <151979534+miguelfordev@users.noreply.github.com>
2025-08-24 18:54:37 -0300

    add interface/implements

--- src/main/java/br/com/mariojp/solid/lspaccounts/SavingsAccount.java
+++ src/main/java/br/com/mariojp/solid/lspaccounts/SavingsAccount.java
@@ -1,8 +1,9 @@
 package br.com.mariojp.solid.lspaccounts;
 
-public class SavingsAccount extends Account {
-    @Override
+public class SavingsAccount extends Account implements iWithdrawable{
+    
     public void withdraw(double amount) {
-        throw new UnsupportedOperationException("Saque não permitido em poupança");
+    	System.out.println("SavingsAccount: Saque ignorado");
+        //throw new UnsupportedOperationException("Saque não permitido em poupança");
     }
 }
---
+++ src/main/java/br/com/mariojp/solid/lspaccounts/iWithdrawable.java
@@ -0,0 +1,7 @@
+package br.com.mariojp.solid.lspaccounts;
+
+public interface iWithdrawable {
+	
+	void withdraw(double amount);
+
+}
